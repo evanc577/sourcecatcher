02f919f02c98b19ea8661b0efc04f6048d925174
Evan Chang <evanc577@gmail.com>
2019-07-03 03:03:06 +0000

    fix init

--- initial.sh
+++ initial.sh
@@ -8,17 +8,17 @@ echo    # (optional) move to a new line
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  rm -f twitter_scraper.db
-  rm -f phash_index.ann
-
-  source ./sourcecatcher_venv/bin/activate
-  python bot.py
-  python gen_phashes.py
-
   LIVE_DIR=live/
   WORKING_DIR=working/
   mkdir -p $LIVE_DIR
   mkdir -p $WORKING_DIR
+
+  rm -f $WORKING_DIR/twitter_scraper.db
+  rm -f $WORKING_DIR/phash_index.ann
+
+  source ./sourcecatcher_venv/bin/activate
+  python bot.py
+  python gen_phashes.py
 
   cp $WORKING_DIR/twitter_scraper.db $LIVE_DIR
   cp $WORKING_DIR/phash_index.ann $LIVE_DIR
