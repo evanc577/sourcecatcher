5ce23bbf2db0ef6831b02cf89f3c6bc06fc4dc8a
Evan Chang <evanc577@gmail.com>
2019-10-11 15:01:14 -0500

    refactor to generate html with templates instead of in pythoon

--- web_server.py
+++ web_server.py
@@ -71,8 +71,17 @@ tweepy_kwargs = {
 @limiter.exempt
 def handle_exception(e):
     """Generic http error handler"""
-    print(e)
     num_photos, num_tweets, mtime= stats()
+
+    if request.full_path == '/' or request.full_path == '/?':
+        kwargs = {
+                'num_photos': num_photos,
+                'num_tweets': num_tweets,
+                'mtime': mtime,
+                }
+        return render_template('sourcecatcher.html', **kwargs)
+
+    print(e)
 
     error_msg = f'<div class="error_code">{e.code} {e.name}</div><br>{e.description}'
     kwargs = {
@@ -85,13 +94,12 @@ def handle_exception(e):
             'results': True,
             'error_msg': error_msg,
             }
-    return render_template('sourcecatcher.html', **kwargs)
+    return render_template('error.html', **kwargs)
 
 @app.errorhandler(413)
 @limiter.exempt
 def entity_too_large(e):
     """Error page if uploaded file is too large"""
-
     num_photos, num_tweets, mtime= stats()
 
     kwargs = {
@@ -104,39 +112,46 @@ def entity_too_large(e):
             'results': True,
             'error_msg': EntityTooLarge().__str__(),
             }
-    return render_template('sourcecatcher.html', **kwargs)
+    return render_template('error.html', **kwargs)
 
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    if request.method == 'POST':
-        f = request.files['file']
-        filename = '{:016x}'.format(random.randint(0, 1<<128))
-        if f.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        f.save(path)
-        html = find_and_render('file', path)
+    f = request.files['file']
+    filename = '{:016x}'.format(random.randint(0, 1<<128))
+    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    f.save(path)
+    html = find_and_render('file', path)
 
-        # remove old files
-        uploads = ['{}/{}'.format(app.config['UPLOAD_FOLDER'], n) for n in os.listdir(app.config['UPLOAD_FOLDER'])]
-        files = sorted(uploads, key=os.path.getctime)
-        if len(files) > 128:
-            os.remove(files[0])
+    # remove old files
+    uploads = ['{}/{}'.format(app.config['UPLOAD_FOLDER'], n) for n in os.listdir(app.config['UPLOAD_FOLDER'])]
+    files = sorted(uploads, key=os.path.getctime)
+    if len(files) > 128:
+        os.remove(files[0])
 
-        return html
-    else:
-        link = request.args.get('link')
-        return find_and_render('url', link)
+    return html
 
 
 @app.route('/')
+@limiter.exempt
 def root():
-    link = request.args.get('link')
-    return find_and_render('url', link)
+    num_photos, num_tweets, mtime= stats()
+    kwargs = {
+            'num_photos': num_photos,
+            'num_tweets': num_tweets,
+            'mtime': mtime,
+            }
+    return render_template('sourcecatcher.html', **kwargs)
+
+
+@app.route('/link')
+def link():
+    url = request.args.get('url')
+    return find_and_render('url', url)
+
 
 def dc_app(path):
+    """Get HQ pictures from DC app"""
     # request DC app webpage
     try:
         response = requests.get(path, timeout=30)
@@ -168,10 +183,18 @@ def dc_app(path):
     # find post username and text
     app_poster = parsed_html.body.find('div', attrs={'class': 'card-name'}).text.strip()
     app_text = parsed_html.body.find('div', attrs={'class': 'card-text'}).text.strip()
+    app_images = list(OrderedDict.fromkeys(files))
 
-    return files, app_poster, app_text
+    kwargs = {}
+    kwargs['app_images'] = app_images
+    kwargs['app_poster'] = app_poster
+    kwargs['app_text'] = app_text
+    kwargs['url'] = path
+
+    return render_template('dc_app.html', **kwargs)
 
 def dc_app_image(path):
+    """Get HQ version of DC app picture"""
     # verify link
     x = re.match(r"((http://|https://)?file\.candlemystar\.com/cache/.*(_\d+x\d+)\.\w+$)", path)
     if x is None:
@@ -183,180 +206,135 @@ def dc_app_image(path):
         image_link = image_link.replace(x.groups()[2], '')
 
         # request image link
-        try:
-            response = requests.get(image_link, timeout=30)
-        except requests.exceptions.MissingSchema:
-            image_link = 'https://' + image_link
-            response = requests.get(image_link, timeout=30)
+        if False:
+            try:
+                response = requests.get(image_link, timeout=30)
+            except requests.exceptions.MissingSchema:
+                image_link = 'https://' + image_link
+                response = requests.get(image_link, timeout=30)
 
-        if response.status_code == 200:
-            app_direct_image = True
-        else:
-            error_msg = 'Error: Image could not be found'
-            raise InvalidDCAppLink
+            if response.status_code == 200:
+                app_direct_image = True
+            else:
+                error_msg = 'Error: Image could not be found'
+                raise InvalidDCAppLink
 
-        return image_link
+
+        app_images = f'<img class="app_img" src={image_link}>\n'
+
+        kwargs = {}
+        kwargs['image_link'] = image_link
+        kwargs['url'] = path
+
+        return render_template('dc_app_image.html', **kwargs)
 
 
 def find_and_render(location, path):
+    """Try to find a matching image and render the results webpage"""
     app = False
     app_direct_image = False
     basename = None
     tweet_id = None
-    embed = None
+    tweets = []
     error_msg = None
 
     num_photos, num_tweets, mtime= stats()
 
+    try:
+        if location == 'url':
+            extract = tldextract.extract(path)
 
-    if path is not None:
-        try:
-            if location == 'url':
-                extract = tldextract.extract(path)
+            if extract.subdomain == 'dreamcatcher' and \
+                    extract.domain == 'candlemystar' and \
+                    extract.suffix == 'com':
+                return dc_app(path)
+            elif extract.subdomain == 'file' and \
+                    extract.domain == 'candlemystar' and \
+                    extract.suffix == 'com':
+                return dc_app_image(path)
+            else:
+                found = find('url', path)
 
-                if extract.subdomain == 'dreamcatcher' and \
-                        extract.domain == 'candlemystar' and \
-                        extract.suffix == 'com':
-                    files, app_poster, app_text = dc_app(path)
-                    app = True
-                elif extract.subdomain == 'file' and \
-                        extract.domain == 'candlemystar' and \
-                        extract.suffix == 'com':
-                    image_link = dc_app_image(path)
-                    app_direct_image = True
-                else:
-                    found = find('url', path)
+        elif location == 'file':
+            found = find('file', path)
 
-            elif location == 'file':
-                found = find('file', path)
+        id_set = set()
+        count = 0
+        for candidate in found:
+            score, tweet_id, basename = candidate
+            if tweet_id in id_set:
+                continue
 
-            if not app and not app_direct_image:
-                id_set = set()
-                count = 0
-                for candidate in found:
-                    score, tweet_id, basename = candidate
-                    if tweet_id in id_set:
-                        continue
+            score_percent = calc_score_percent(score)
 
-                    score_percent = calc_score_percent(score)
+            tweets.append(get_custom_embed(tweet_id, score_percent))
 
-                    if count == 0:
-                        embed = ""
+            id_set.add(tweet_id)
+            count += 1
 
-                    embed += get_custom_embed(tweet_id, score_percent)
+        if count == 0:
+            raise NoMatchesFound
 
-                    id_set.add(tweet_id)
-                    count += 1
+    except SCError as e:
+        error_msg = str(e)
+        print(e)
 
-                if count == 0:
-                    raise NoMatchesFound
-
-        except SCError as e:
-            error_msg = str(e)
-            print(e)
-
-        except Exception as e:
-            error_msg = "An unknown error occurred"
-            print(e)
+    except Exception as e:
+        error_msg = "An unknown error occurred"
+        print(e)
 
     kwargs = {
-            'embed': embed,
+            'tweets': tweets,
             'num_photos': num_photos,
             'num_tweets': num_tweets,
             'mtime': mtime,
-            'app': app,
-            'app_direct_image': app_direct_image,
             'error_msg': error_msg,
             }
 
     if location == 'url':
-        kwargs['link'] = path
+        kwargs['url'] = path
 
-    if app:
-        files = list(OrderedDict.fromkeys(files))
-        app_images = ''
-        app_images += f'<h3>{app_poster}:</h3>\n'
-        app_images += f'<p>{app_text}<p>\n'
-        for f in files:
-            app_images += f'<img class="app_img" src={f}>\n'
-        kwargs['app_images'] = app_images
+    # found some matches
+    if len(tweets) != 0:
+        return render_template('match_results.html', **kwargs)
 
-    if app_direct_image:
-        app_images = f'<img class="app_img" src={image_link}>\n'
-        kwargs['app_images'] = app_images
-
-    if path is not None:
-        kwargs['results'] = True
-
-    return render_template('sourcecatcher.html', **kwargs)
-
-def add_result_title(html, tweet_source):
-    header = '<div class="result">\n<div class="result_title">\n<a href={0} ">{0}</a>'.format(tweet_source)
-    footer = '\n</div>'
-    return header + html + footer
+    # did not find any matches
+    return render_template('error.html', **kwargs)
 
 def get_custom_embed(tweet_id, score):
     """
     Create a custom embedded tweet
     """
+
+    tweet = {}
+    tweet['custom'] = True
+    tweet['tweet_id'] = tweet_id
+    tweet['score'] = score
     try:
         # get tweet contents
         status = api.get_status(tweet_id, **tweepy_kwargs)
 
         # process tweet text
         display_range = status._json['display_text_range']
-        text = status._json['full_text'][display_range[0]:display_range[1]]
-        text_html = escape(text).replace('\n', '<br />')
+        tweet['text_html'] = status._json['full_text'][display_range[0]:display_range[1]]
 
         # process name
-        screen_name = status._json['user']['screen_name']
-        identity_name = status._json['user']['name']
-        profile_image = status._json['user']['profile_image_url_https']
+        tweet['screen_name'] = status._json['user']['screen_name']
+        tweet['identity_name'] = status._json['user']['name']
+        tweet['profile_image'] = status._json['user']['profile_image_url_https']
 
         # process time
-        ts = status._json['created_at']
+        tweet['ts'] = status._json['created_at']
 
         # process tweet images
         media = status._json['extended_entities']['media']
-        num_media = len(media)
-        images_html = ""
+        tweet['num_media'] = len(media)
+        images = []
         for m in media:
-            url = m['media_url_https']
-            images_html += f'<div class="image_container num_media{num_media}">\n<img alt="Twitter image" src="{url}">\n</div>\n'
+            images.append(m['media_url_https'])
+        tweet['images'] = images
 
-        html = f'''
-<a class="tweet_embed" target="_blank" rel="noopener noreferrer" title="View on Twitter" href="https://twitter.com/{screen_name}/status/{tweet_id}">
-  <div class="match_score">Match Score: {score}</div>
-  <img class="twitter_logo" src="static/Twitter_Logo_Blue.svg">
-  <div class="author">
-    <img class="avatar" alt="Avatar" src="{profile_image}">
-    <div class="name_container">
-      <span class="identity_name">
-        {identity_name}
-      </span>
-      <span class="screen_name">
-        @{screen_name}
-      </span>
-    </div>
-  </div>
-  <div class="datetime">
-    <script>datetime = new Date("{ts}");
-      datestr = datetime.toLocaleDateString();
-      timestr = datetime.toLocaleTimeString();
-      datetimestr = timestr + " - " + datestr;
-      document.write(datetimestr)
-    </script>
-  </div>
-  <div class="tweet_text">
-    {text_html}
-  </div>
-  <div class="tweet_images">
-    {images_html}
-  </div>
-</a>
-'''
-
-        return html
+        return tweet
     except Exception as e:
         # custom embed failed for some reason, try Twitter's official embed
         print(f"Error creating custom embedded tweet: {e}")
@@ -364,13 +342,15 @@ def get_custom_embed(tweet_id, score):
 
 def get_embed(tweet_id):
     """get html for an embedded tweet"""
+    tweet = {}
+    tweet['custom'] = False
     tweet_source = 'https://www.twitter.com/a/status/{}'.format(tweet_id)
     url = urllib.parse.quote(tweet_source, safe='')
     get_url = 'https://publish.twitter.com/oembed?url={}'.format(url)
 
     r = requests.get(url=get_url, timeout=30)
-    html = r.json()['html'] + '\n'
-    return html
+    tweet['embed_tweet'] = r.json()['html']
+    return tweet
 
 def calc_score_percent(score):
     """calculate the percentage score, where 100 is best and 0 is worst"""
@@ -378,20 +358,3 @@ def calc_score_percent(score):
         return 0
 
     return int(100 - 100 * score / 32)
-
-def remove_scripts(html):
-    """experimental: remove scripts from html"""
-    begin = '<script'
-    end = '</script>'
-    idx1 = html.find(begin)
-    if idx1 == -1:
-        return html
-
-    idx2 = html.find(end)
-    if idx2 == -1:
-        html = html[:idx1]
-    else:
-        idx2 = idx2 + len(end)
-        html = html[:idx1] + html[idx2:]
-
-    return html
