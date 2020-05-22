b190fa33c06f0f040138828a3f1ceae5039b2775
Evan Chang <evanc577@gmail.com>
2020-05-22 00:56:21 -0400

    Set max backups to keep via SOURCECATCHER_NUM_BACKUPS env var

--- prune_backups.sh
+++ prune_backups.sh
@@ -2,8 +2,14 @@
 
 idx=0
 
+if [[ -z "${SOURCECATCHER_NUM_BACKUPS}" ]]; then
+  NUM_BACKUPS="10"
+else
+  NUM_BACKUPS="${SOURCECATCHER_NUM_BACKUPS}"
+fi
+
 for backup in $(find backups/ -mindepth 1 -maxdepth 1 | sort -r); do
-  if [ $idx -lt 10 ]; then
+  if [ $idx -lt ${NUM_BACKUPS} ]; then
     ((++idx))
   else
     echo "Removing backup $backup"
