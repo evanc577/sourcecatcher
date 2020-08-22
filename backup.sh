6176c8f6e04bee4607fb41b5d1abac1f8b277e3b
Evan Chang <evanc577@gmail.com>
2020-08-22 18:22:14 -0400

    fix: backup descriptors database

--- backup.sh
+++ backup.sh
@@ -16,7 +16,7 @@ echo "acquired lock"
 BACKUP_FILE=backups/$(date --iso-8601=seconds)
 LIVE_DIR=live
 
-tar -cv config.yaml $LIVE_DIR/twitter_scraper.db $LIVE_DIR/phash_index.ann $LIVE_DIR/descriptors.pkl $LIVE_DIR/BOW_annoy_map.pkl $LIVE_DIR/kmeans.pkl $LIVE_DIR/BOW_index.ann 2>/dev/null | pigz > $BACKUP_FILE.tar.gz
+tar -cv config.yaml $LIVE_DIR/twitter_scraper.db $LIVE_DIR/phash_index.ann $LIVE_DIR/descriptors.bdb $LIVE_DIR/BOW_annoy_map.pkl $LIVE_DIR/kmeans.pkl $LIVE_DIR/BOW_index.ann 2>/dev/null | pigz > $BACKUP_FILE.tar.gz
 
 echo "Backed up to $BACKUP_FILE.tar.gz"
 
