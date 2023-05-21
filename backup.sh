f76c8446b4cb1641e3561656be5a23992a566e26
Evan Chang <evanc577@gmail.com>
2023-05-21 18:06:22 -0400

    Stream backup files

--- backup.sh
+++ backup.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+if [ -z "$BACKUP_SERVER" ]; then
+  echo "BACKUP_SERVER unset"
+  exit 1
+fi
+if [ -z "$BACKUP_SERVER_USER" ]; then
+  echo "BACKUP_SERVER_USER unset"
+  exit 1
+fi
+if [ -z "$BACKUP_SERVER_PASS" ]; then
+  echo "BACKUP_SERVER_PASS unset"
+  exit 1
+fi
+
 LOCK=/tmp/sourcecatcher.lock
 function cleanup {
   rm -rf $LOCK
@@ -13,11 +26,11 @@ while ! mkdir $LOCK 2> /dev/null; do
 done
 echo "acquired lock"
 
-BACKUP_FILE=backups/$(date --iso-8601=seconds)
+BACKUP_FILE=$(date --iso-8601=seconds).tar.gz
 LIVE_DIR=live
 
-tar -cv config.yaml $LIVE_DIR/twitter_scraper.db $LIVE_DIR/phash_index.ann $LIVE_DIR/descriptors.bdb $LIVE_DIR/BOW_annoy_map.pkl $LIVE_DIR/kmeans.pkl $LIVE_DIR/BOW_index.ann 2>/dev/null | pigz > $BACKUP_FILE.tar.gz
+tar -cv config.yaml $LIVE_DIR/twitter_scraper.db $LIVE_DIR/phash_index.ann $LIVE_DIR/descriptors.bdb $LIVE_DIR/BOW_annoy_map.pkl $LIVE_DIR/kmeans.pkl $LIVE_DIR/BOW_index.ann 2>/dev/null \
+  | pigz \
+  | curl -T - -u "$BACKUP_SERVER_USER:$BACKUP_SERVER_PASS" -H "X-FILE: $BACKUP_FILE" $BACKUP_SERVER
 
-echo "Backed up to $BACKUP_FILE.tar.gz"
-
-./prune_backups.sh
+echo "Backed up to $BACKUP_SERVER"
